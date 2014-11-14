ad432c6f97592c8dfdae46390b06c480d4d14b4d
alok <alokin.ntl@gmail.com>
2014-11-14 18:07:01 +0530

    Update init.pp

--- modules/old_module/nginx/manifests/init.pp
+++ modules/old_module/nginx/manifests/init.pp
@@ -1,7 +1,4 @@
 class nginx {
-exec { 'apt-update':                    
-  command => '/usr/bin/apt-get update'  
-}
 package { 'nginx':
   require => Exec['apt-update'],        
   ensure => installed,
