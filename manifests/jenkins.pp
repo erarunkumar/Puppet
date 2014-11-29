f039535775e630eea0cbe4a23b1d5c9b71adca08
alok <alokin.ntl@gmail.com>
2014-11-29 16:46:45 +0530

    Update jenkins.pp

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,1 +1,12 @@
+
+#Use this command to install jenkins
+puppet module install rtyler-jenkins
+
+#Install jenkins
 include jenkins
+
+#Install jenkins 
+jenkins::plugin {
+  "git" :
+    version => "1.1.11";
+}
