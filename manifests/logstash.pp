e5c9b8a8515a554e10a44fc5ca9f01fe12e19c7f
alok <alokin.ntl@gmail.com>
2014-12-01 12:52:28 +0530

    Update logstash.pp

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -1,2 +1,38 @@
-include logstash
+#Use this command to install logstash
+puppet module install elasticsearch-logstash
 
+#run these command
+
+echo 'deb http://packages.elasticsearch.org/logstash/1.4/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
+
+apt-key advrver keyserver.ubuntu.com --recv-keys D27D666CD88E42B4
+
+apt-get update
+
+#install logstash
+class { 'logstash': }
+
+#upgrade Logstash
+class { 'logstash':
+  autoupgrade => true
+}
+
+#Disable and remove Logstash entirely
+ class { 'logstash':
+  ensure => 'absent'
+}
+
+# add init file
+class { 'logstash':
+  init_defaults_file => '/etc/defaults/logstash'
+  }
+  
+  #Contrib package installation
+  class { 'logstash':
+  install_contrib => true
+}
+
+#Java Install
+class { 'logstash':
+  java_install => true
+}
