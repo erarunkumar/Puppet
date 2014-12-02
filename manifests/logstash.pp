22cab14247d807256f01eb48690367e80988014a
alok <alokin.ntl@gmail.com>
2014-12-02 15:48:46 +0530

    Update logstash.pp

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -5,7 +5,9 @@ puppet module install elasticsearch-logstash
 
 echo 'deb http://packages.elasticsearch.org/logstash/1.4/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
 
-apt-key advrver keyserver.ubuntu.com --recv-keys D27D666CD88E42B4
+#apt-key advrver keyserver.ubuntu.com --recv-keys D27D666CD88E42B4
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D27D666CD88E42B4
+
 
 apt-get update
 
