46e42aa6a35285a9b69775074783836141bd6608
root <root@vagrant-centos64.vagrantup.com>
2015-01-08 17:25:26 +0000

    Added snoopy puppet module

---
+++ modules/snoopy/manifests/init.pp
@@ -0,0 +1,37 @@
+class snoopy (
+    $logger     = "syslog-ng",
+    $logfile    = "/var/log/snoopy.log",
+    ) {
+	file{ ["/etc/syslog-ng","/etc/syslog-ng/conf.d"]:
+	    ensure => directory,
+	    mode => "0755"
+	}
+	package { "syslog-ng":
+            ensure => 'present'
+        }
+        package { "snoopy":
+            ensure => 'installed'
+        }
+        Exec {
+            require => Package['snoopy']
+        }
+        exec { "snoopy_add_preload":
+            command     => "/bin/echo '/lib64/snoopy.so' >> /etc/ld.so.preload",
+            unless      => "/bin/grep snoopy /etc/ld.so.preload"
+        }
+        file { "/etc/logrotate.d/snoopy":
+            content => template('snoopy/logrotate.conf.erb'),
+        }
+        # Process logging
+        if $logger == 'syslog-ng' {
+            file { "/etc/syslog-ng/conf.d/snoopy.conf":
+                content => template('snoopy/syslog-ng.conf.erb'),
+                alias   => "logger",
+            }
+        }
+        exec { "logger-restart":
+            command     => "/etc/init.d/$logger restart",
+            subscribe   => File["logger"],
+            refreshonly => true,
+        }
+}
