9eb52baa2d2f85ca2950b25c311dd25fa0daaca1
alok <alokin.ntl@gmail.com>
2015-02-25 14:05:57 +0530

    Update jenkins.pp

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -2,13 +2,41 @@
 #Use this command to install jenkins
 puppet module install rtyler-jenkins
 
-#Install jenkins
+Exec {
+     path => [
+             '/usr/local/bin',
+             '/opt/local/bin',
+             '/usr/bin',
+             '/usr/sbin',
+             '/bin',
+             '/sbin'],
+             logoutput => true,
+}
+
 include jenkins
 
-#Install jenkins plugin
 jenkins::plugin {
-  "git" : 
-  version => "1.1.11";
+  "git" : ;
+  "parameterized-trigger" : ;
+  "token-macro" : ;
+  "mailer" : ;
+  "scm-api" : ;
+  "promoted-builds" : ;
+  "matrix-project" : ;
+  "git-client" : ;
+  "ssh-credentials" : ;
+  "credentials" : ;
+}
+
+jenkins::user {'opstree':
+  full_name => 'opstree',
+  email    => 'opstree@gmail.com',
+  password => 'password',
+  before => Class['jenkins::security'],
+}
+
+class{'jenkins::security':
+   security_model => 'full_control',
 }
 
 
