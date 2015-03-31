52234c3e69cc27b9ba71bbd680158803c565e4b1
Sandy <sandeep_r80@yahoo.co.in>
2015-03-31 14:07:13 +0530

    Added a module to manage selinux

---
+++ modules/manage-selinux/manifests/init.pp
@@ -0,0 +1,28 @@
+class manage-selinux ($selinuxState="Permissive") {
+
+	case $selinuxState {
+	        'Permissive': {
+        	        $seStatus=0
+                	notify { "setting status for $selinuxState": }
+	        }
+        	'Enforce': {
+                	notify { "setting status for $selinuxState": }
+	                $seStatus=1
+        	}
+	}
+
+	exec { 'selinux-enforce' :
+		#If state is not alread set, setting it
+		command => "/usr/sbin/setenforce $seStatus",
+		unless => "/usr/sbin/getenforce | grep $selinuxState 2>/dev/null",    
+		logoutput => "true",
+	}
+
+	file { 'selinux-config':
+		path => '/etc/selinux/config',
+		ensure => file,
+		content => template("manage-selinux/selinux_config.erb"), 
+	}	
+
+}
+
