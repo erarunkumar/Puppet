c6ed11f9761137d55af2ed7a88f69905d714d52e
root <root@server2.example.com>
2015-04-09 14:20:00 +0530

    Added updated gluster module

---
+++ saurabh/gluster/manifests/peers.pp
@@ -0,0 +1,20 @@
+class gluster::peers(
+$peers = []
+){
+Exec {
+path => [
+'/usr/local/bin',
+'/opt/local/bin',
+'/usr/bin',
+'/usr/sbin',
+'/bin',
+'/sbin'],
+logoutput => true,
+}
+define myResource {
+exec { "gluster peer probe $name":
+unless => "/bin/egrep '^hostname.+=${name}$' /var/lib/glusterd/peers/*"
+}
+}
+myResource { $peers: }
+}
