b62dc7aa476700433e9cc86035a0569c3ff58e09
saurabhvaj <saurabhvajpayee2010@gmail.com>
2015-04-23 07:38:02 +0000

    sftpjail module

--- saurabh/sftpjail/manifests/client.pp
+++ saurabh/sftpjail/manifests/client.pp
@@ -1,9 +1,8 @@
 class sftpjail::client(
- $group_name = '',
- $user_name = '',
- $groups = [],
- $homedir = '',
- $u_pass = '',
+$group_name = '',
+$user_pass = [],
+$user_groups = [],
+$homedir = ''
 
 ){
 
@@ -20,53 +19,51 @@ class sftpjail::client(
                                   logoutput => true,
                       }
 
-                  group {"$group_name":
-                         ensure => present, 
-                        }
-
-                  user {"${user_name}": 
-                         # password => generate('/bin/sh', '-c', "mkpasswd -m sha-512 ${u_pass} | tr -d '\n'"),
-                         groups => "$groups",
-                         home => "$homedir",
-                         #before => File['/sftp/${user_name}']
-                       }
-                 exec{"userpassword":
-                         command => "bash -c  'echo -e \"$u_pass\n$u_pass\" | passwd $user_name'",
-                         require => User[$user_name]
-                      }
-
-                 file_line {"config_file1":
+                 package{"openssh-server":
+                               ensure => present
+                 }
+                 file_line {"internal_sftp":
                        path => "/etc/ssh/sshd_config",
                        ensure => present,
-                       line => "Subsystem sftp internal-sftp",
-                       match => "Subsystem sftp /usr/lib/openssh/sftp-server",
-                      }
+                     line => "Subsystem sftp internal-sftp",
+                      match => "Subsystem sftp /usr/lib/openssh/sftp-server",
+                       require => Package['openssh-server']
+                     }
  
-                 file_line {"config_file2":
+                 file_line {"match_group":
                        path => "/etc/ssh/sshd_config",
                        ensure => present,
                        line => "Match Group $group_name
         ChrootDirectory /sftp/%u
         ForceCommand internal-sftp",
-                      require => File_line['config_file1']
-                      }  
-                 
-                  $mkdir =["/sftp/","/sftp/$user_name"]
-                      
-                 file{$mkdir:
-                        ensure => directory,
-                        recurse => true                        
-                     }
-                 
-                 file{"userroot":
-                         path => "/sftp/$user_name/incoming",
-                         ensure => directory,
-                         group => "$group_name",
-                         owner => "$user_name"
-                     }
-                 
-                 exec{"restartservice":
-                     command => "service ssh restart",
-                     }
+                       match => "Match Group $group_name
+        ChrootDirectory /sftp/%u
+        ForceCommand internal-sftp",
+                      require => File_line['internal_sftp'],
+                     notify  => Service["ssh"]
+                    }
+
+
+
+
+
+
+
+                 service { "ssh":
+                            ensure  => "running",
+                            enable  => "true",
+                            require => Package["openssh-server"],
+                         }
+
+
+
+
+
+                class{"sftpjail::user":
+                                   homedir => "$homedir",
+                                   group_name => "$group_name",
+                                   user_pass => $user_pass
+                    }
+
 }
 
