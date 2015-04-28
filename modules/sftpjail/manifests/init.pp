238d1f58ca1531dd008c33738c8e2b385dcd6aed
saurabhvaj <saurabhvajpayee2010@gmail.com>
2015-04-28 05:58:31 +0000

    changes with default parameters

--- modules/sftpjail/manifests/init.pp
+++ modules/sftpjail/manifests/init.pp
@@ -6,14 +6,14 @@
 # Copyright 2015 opstree.
 #
 class sftpjail( 
-$homedir = '',
-$group_name = "",
-$user_pass = []
-){
+               $homedir      = $sftpjail::params::homedir,
+               $group_name   = $sftpjail::params::group_name,
+               $user_pass    = $sftpjail::params::user_pass
+               )inherits sftpjail::params{
 
                    class { 'sftpjail::client': 
                                        group_name => "$group_name",
-                                      user_pass => $user_pass,
+                                       user_pass => $user_pass,
                                        homedir => "$homedir"
 
                     } 
