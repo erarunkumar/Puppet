e84c4ae4f1785f31ea5884a694078ed35afd7224
nu113r <belwal.mohit@gmail.com>
2015-05-19 07:12:27 +0000

    Formatted module

--- modules/sftpjail/manifests/client.pp
+++ modules/sftpjail/manifests/client.pp
@@ -3,50 +3,46 @@ $group_name = '',
 $user_pass = [],
 $user_groups = [],
 $homedir = ''
-
 ){
+  include stdlib
+  include sftpjail::service
+  Exec {
+    path => [
+            '/usr/local/bin',
+            '/opt/local/bin',
+            '/usr/bin',
+            '/usr/sbin',
+            '/bin',
+            '/sbin'],
+            logoutput => true,
+  }
 
-                 include stdlib
-                 include sftpjail::service
+  package{"openssh-server":
+    ensure => present
+  }
 
-                 Exec {
-                        path => [
-                                  '/usr/local/bin',
-                                  '/opt/local/bin',
-                                  '/usr/bin',
-                                  '/usr/sbin',
-                                  '/bin',
-                                  '/sbin'],
-                                  logoutput => true,
-                      }
+  file_line {"internal_sftp":
+    path => "/etc/ssh/sshd_config",
+    ensure => present,
+    line => "Subsystem sftp internal-sftp",
+    match => "Subsystem sftp /usr/lib/openssh/sftp-server",
+    require => Package['openssh-server']
+  }
 
-                 package{"openssh-server":
-                               ensure => present
-                 }
-                 file_line {"internal_sftp":
-                       path => "/etc/ssh/sshd_config",
-                       ensure => present,
-                     line => "Subsystem sftp internal-sftp",
-                      match => "Subsystem sftp /usr/lib/openssh/sftp-server",
-                       require => Package['openssh-server']
-                     }
-
-                exec{"groupname":
+  exec{"groupname":
                        command => "echo 'Match Group $group_name
         ChrootDirectory /sftp/%u
         ForceCommand internal-sftp' >> /etc/ssh/sshd_config",
                        unless => "grep $group_name /etc/ssh/sshd_config",
                        require => File_line['internal_sftp'],
                        notify  => Class['sftpjail::service']
-                    }
- 
+  }
 
-
-                class{"sftpjail::user":
-                       homedir => "$homedir",
-                       group_name => "$group_name",
-                       user_pass => $user_pass
-                    }
+  class{"sftpjail::user":
+    homedir => "$homedir",
+    group_name => "$group_name",
+    user_pass => $user_pass
+  }
 
 }
 
