28ac34ba15fdc074116da1c8b99f2e162757536f
nu113r <belwal.mohit@gmail.com>
2015-05-19 07:12:41 +0000

    Formatted module

--- modules/sftpjail/manifests/init.pp
+++ modules/sftpjail/manifests/init.pp
@@ -6,17 +6,15 @@
 # Copyright 2015 opstree.
 #
 class sftpjail( 
-               $homedir      = $sftpjail::params::homedir,
-               $group_name   = $sftpjail::params::group_name,
-               $user_pass    = $sftpjail::params::user_pass
-               )inherits sftpjail::params{
+$homedir      = $sftpjail::params::homedir,
+$group_name   = $sftpjail::params::group_name,
+$user_pass    = $sftpjail::params::user_pass
+)inherits sftpjail::params{
 
-                   class { 'sftpjail::client': 
-                                       group_name => "$group_name",
-                                       user_pass => $user_pass,
-                                       homedir => "$homedir"
+  class { 'sftpjail::client': 
+    group_name => "$group_name",
+    user_pass => $user_pass,
+    homedir => "$homedir"
+  } 
 
-                    } 
-
-
-                  }
+}
