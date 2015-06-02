b52db1152ba8ae3274965c682f0b09e91b59b8cd
root <root@ubuntu-14.04-amd64-vbox>
2015-06-02 12:35:10 +0000

    adding keybase authentication

--- modules/sftpjail/manifests/user.pp
+++ modules/sftpjail/manifests/user.pp
@@ -1,23 +1,58 @@
-define sftpjail::user($name=undef,$password=undef) {
+define sftpjail::user($name=undef,$password=undef,$udt=undef,$authorized_keys=undef) {
+Exec {
+    path => [
+            '/usr/local/bin',
+            '/opt/local/bin',
+            '/usr/bin',
+            '/usr/sbin',
+            '/bin',
+            '/sbin'],
+            logoutput => true,
+  }
 
+if $udt {
 	user { $name: 
 		password => "$password",
 		managehome => true,
 		ensure => present,
-		home => "/incoming",
+		home   => "/incoming",
 		groups => ['sftpusers'],
-		shell => '/sbin/nologin'
 	}
-
 	file { ["/sftp/$name"]:
-		ensure =>"directory",
-	}
-
-	file { ["/sftp/$name/incoming"]:
-                ensure => "directory",
-		owner => "$name",
-		group => "sftpusers"
+                ensure =>"directory",
         }
-		
-}
 
+        file { ["/sftp/$name/incoming"]:
+                ensure => "directory",
+                owner  => "$name",
+                group  => "sftpusers"
+        }
+	}
+else{
+	user {$name:
+                managehome => true,
+                ensure => present,
+                home   => "/incoming",
+                groups => ['sftpusers'],
+        }
+
+	exec{"authorized_keys":
+		command => "bash -c 'echo $authorized_keys >> /etc/ssh-pool/$name.pub'",
+		require => File['/etc/ssh-pool']
+	}
+        file { ["/sftp/$name"]:
+                ensure =>"directory",
+        }
+
+        file { ["/sftp/$name/incoming"]:
+                ensure => "directory",
+                owner  => "$name",
+                group  => "sftpusers"
+        }
+        file { ["/etc/ssh-pool"]:
+                ensure => "directory",
+                owner  => "$name",
+                group  => "sftpusers",
+	}
+}
+}
