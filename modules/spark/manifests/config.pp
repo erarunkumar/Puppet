eab1d7a384a6edcc008965ff8b84e0cca7c98946
Sandy <sandeep@panthacorp.com>
2017-02-13 22:54:17 +0530

    .

--- modules/spark/manifests/config.pp
+++ modules/spark/manifests/config.pp
@@ -1,31 +1,33 @@
+# == Class: spark::config
+#
+# Full description of class a here.
+#
 class spark::config (
-	$dirname = 'spark-2.1.0-bin-hadoop2.7',
-	$filename = "${dirname}.tgz",
-	$install_path = "/home/spark/${dirname}"
+  $dirname      = 'spark-2.1.0-bin-hadoop2.7',
+  $filename     = "${dirname}.tgz",
+  $install_path = "/home/spark/${dirname}"
 ){
-
-	user { 'spark':
- 		ensure           => 'present',
- 		home             => '/home/spark',
-       	shell            => '/bin/bash',
-       	managehome     	 => true,
-    }
-
-	archive { $filename:
-  		path          => "/tmp/${filename}",
-	  	source        => 'https://archive.apache.org/dist/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz',
-	  	checksum_type => 'none',
-	  	extract       => true,
-	  	extract_path  => '/home/spark',
-	  	creates       => "${install_path}/bin",
-	  	cleanup       => true,
-	  	user          => 'spark',
-	  	group         => 'spark',
-	  	require       => User['spark'],
-	}
-	file { '/home/spark/spark':
-  		ensure => link,
-  		target => "/home/spark/${dirname}",
-  		require       => Archive["$filename"],
-	}
+  user { 'spark':
+    ensure     => 'present',
+    home       => '/home/spark',
+    shell      => '/bin/bash',
+    managehome => true,
+  }
+  archive { $::filename:
+    path          => "/tmp/${filename}",
+    source        => 'https://archive.apache.org/dist/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz',
+    checksum_type => 'none',
+    extract       => true,
+    extract_path  => '/home/spark',
+    creates       => "${install_path}/bin",
+    cleanup       => true,
+    user          => 'spark',
+    group         => 'spark',
+    require       => User['spark'],
+  }
+  file { '/home/spark/spark':
+    ensure  => link,
+    target  => "/home/spark/${dirname}",
+    require => Archive[$::filename],
+  }
 }
--- modules/spark/manifests/init.pp
+++ modules/spark/manifests/init.pp
@@ -3,33 +3,34 @@
 # Full description of class a here.
 #
 class spark (
-	$spark_env = undef,
-	$slave = undef,
-) inherits spark::params {
-	include spark::config
+  $spark_env = undef,
+  $slave = undef,
+) {
+  include spark::params
+  include spark::config
 
-	if $slave != undef {
-  		file { "$confdir/slaves":
-    		content => template('spark/slaves.erb'),
-    		mode    => '0644',
-    		owner   => 'spark',
-    		group   => 'spark',
-  		}
-  	}
+  if $::slave != undef {
+    file { "${::confdir}/slaves":
+      content => template('spark/slaves.erb'),
+      mode    => '0644',
+      owner   => 'spark',
+      group   => 'spark',
+    }
+  }
 
-  	if $spark_env != undef {
-  	  	file { "$confdir/spark-env.sh":
-    		content => template('spark/spark-env.sh.erb'),
-    		mode    => '0755',
-    		owner   => 'spark',
-    		group   => 'spark',
-  		}
-  	}
+  if $::spark_env != undef {
+    file { "${::confdir}/spark-env.sh":
+      content => template('spark/spark-env.sh.erb'),
+      mode    => '0755',
+      owner   => 'spark',
+      group   => 'spark',
+    }
+  }
 
-  	file { '/etc/profile.d/spark.sh':
-    	source  => 'puppet:///modules/spark/profile.sh',
-    	owner   => 'root',
-    	group   => 'root',
-    	mode    => '0755',
-  	}
+  file { '/etc/profile.d/spark.sh':
+    source => 'puppet:///modules/spark/profile.sh',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
 }
