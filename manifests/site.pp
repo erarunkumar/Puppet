612948d3de7c4ebf65e4560320d2120ab89d3cbd
Sandy <sandeep@panthacorp.com>
2017-04-01 12:17:54 +0530

    .

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,35 +1,10 @@
-node /^adtech-cassandra-\w+\.internal$/ {
+node /^spark-node-\d+\.internal$/ {
   include roles::spark
 }
-
-node /^adtech-spark-\w+\.internal$/ {
-  include roles::spark
-}
-
-node /^adtech-adserver-\w+\.internal$/ {
-  include roles::java
-}
-
-node /^adtech-aerospike-\w+\.internal$/ {
-  include roles::aerospike
-}
-
-node /^adtech-frontend-\w+\.internal$/ {
-  include roles::nodejs
-}
-
-node /^adtech-zookeeper-\w+\.internal$/ {
+node /^zookeeper-node-\d+\.internal$/ {
   include roles::zookeeper
 }
 
-node /^adtech-backend-\w+\.internal$/ {
-  include roles::java
-}
-
-node /^adtech-kafka-\w+\.internal$/ {
-  include roles::kafka
-}
-
-node /^adtech-cassandra-\w+\.internal$/ {
-  include roles::kafka
+node /^cassandra-node-\d+\.internal$/ {
+  include roles::cassandra
 }
