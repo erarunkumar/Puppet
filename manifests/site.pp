c9892dfa5b71102cf032765eb75a6e414ea64d4c
Sandy <sandeep@panthacorp.com>
2017-04-01 12:28:15 +0530

    added kafka

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,15 @@
 node /^spark-node-\d+\.internal$/ {
   include roles::spark
 }
+
 node /^zookeeper-node-\d+\.internal$/ {
   include roles::zookeeper
 }
 
 node /^cassandra-node-\d+\.internal$/ {
   include roles::cassandra
+}
+
+node /^kafka-node-\d+\.internal$/ {
+  include roles::kafka
 }
