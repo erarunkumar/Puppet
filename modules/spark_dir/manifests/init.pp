b42b6eb1ee5360e093aa6eecf297f2592820e9b9
Sandy <sandeep@panthacorp.com>
2017-04-04 16:00:50 +0530

    .

--- modules/spark_dir/manifests/init.pp
+++ modules/spark_dir/manifests/init.pp
@@ -3,7 +3,7 @@ class spark_dir {
     file { ['/data','/data/mobiadz','/data/mobiadz/spark/','/data/mobiadz/spark/analytics', '/data/mobiadz/spark/apiserver']:
       ensure  => directory,
       owner   => 'spark',
-      group   => 'app_user',
+      group   => 'spark',
       require => Class['spark']
     }
 }
