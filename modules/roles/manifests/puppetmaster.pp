162dcd6e2d8d90e9235341254cc5ed45e40335b4
Sandy <sandeep@panthacorp.com>
2017-04-20 10:59:16 +0530

    .

--- modules/roles/manifests/puppetmaster.pp
+++ modules/roles/manifests/puppetmaster.pp
@@ -3,7 +3,7 @@
 class roles::puppetmaster {
   include profiles::base
   include profiles::puppetmaster
-  class {'spark::spark_pub_keysshkey':
+  class {'spark::spark_pub_key':
     require => Class['profiles::puppetmaster']
   }
 }
