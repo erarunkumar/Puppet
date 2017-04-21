3745eca012a5d3e554e182603bdaed8cabd725e0
Sandy <sandeep@panthacorp.com>
2017-04-21 13:52:04 +0530

    ebs mount for cassandra

--- modules/roles/manifests/cassandra.pp
+++ modules/roles/manifests/cassandra.pp
@@ -1,7 +1,10 @@
 # == Class: class_name
 #
 class roles::cassandra {
+    class { 'profiles::fstab': }->
     class { 'profiles::base': }->
-    class { 'profiles::java': }->
+    class { 'profiles::java':
+      before => Class['profiles::cassandra']
+    }
     class { 'profiles::cassandra': }
 }
