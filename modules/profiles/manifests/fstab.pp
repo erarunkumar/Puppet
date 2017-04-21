0aba4cc7cebc1bad7b811e3485a650bfecc5b0e1
Sandy <sandeep@panthacorp.com>
2017-04-21 10:58:07 +0530

    .

--- modules/profiles/manifests/fstab.pp
+++ modules/profiles/manifests/fstab.pp
@@ -1,11 +1,17 @@
 # == Class: profiles::fstab
 #
 class profiles::fstab {
-  mount { "/mnt/data":
-      device  => "/dev/sdb1",
-      fstype  => "ext4",
-      ensure  => "mounted",
-      options => "defaults",
-      atboot  => "true",
+  filesystem { '/dev/xvdb':
+    ensure  => present,
+    fs_type => 'xfs',
+    options => '-b 4096 -E stride=32,stripe-width=64',
+  }
+  mounts { 'Mount point for ext4':
+    ensure  => present,
+    source  => '/dev/xvdb',
+    dest    => '/mnt/',
+    type    => 'xfs',
+    opts    => 'nofail,defaults,noatime',
+    require => Class['filesystem']
   }
 }
