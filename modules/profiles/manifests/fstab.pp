827259d58aeefab32892f0736cc6a69d07623ecd
Sandy <sandeep@panthacorp.com>
2017-04-21 11:08:47 +0530

    .

--- modules/profiles/manifests/fstab.pp
+++ modules/profiles/manifests/fstab.pp
@@ -3,12 +3,12 @@
 class profiles::fstab {
   exec {'filesystem':
     command => '/sbin/mkfs.xfs /dev/xvdb',
-    unless  => '/sbin/blkid -t TYPE=ext4 /dev/vdc'
+    unless  => '/sbin/blkid -t TYPE=xfs /dev/xvdb'
   }
   mounts { 'Mount point for ext4':
     ensure  => present,
     source  => '/dev/xvdb',
-    dest    => '/mnt/',
+    dest    => '/mnt/data',
     type    => 'xfs',
     opts    => 'nofail,defaults,noatime',
     require => Exec['filesystem']
